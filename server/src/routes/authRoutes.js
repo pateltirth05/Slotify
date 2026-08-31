e9108462dcb02d0bb4fa24b710a260aad8dac94d
Tirth Patel <pateltirth1025@gmail.com>
2026-08-31 13:07:42 +0530

    Authorization added

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,9 +1,23 @@
 import express from "express"
 
 import { registerUser,login } from "../controllers/authController.js"
-
+import { protect } from "../middleware/authMiddleware.js"
+import { requireRole } from "../middleware/roleMiddleware.js"
 const router=express.Router()
 
 router.post("/register",registerUser)
 router.post("/login",login)
+// router.get("/me",protect,(req,res)=>{
+//     res.status(200).json({
+//         success:true,
+//         user:req.user
+//     })
+// })
+router.get("/owner-test", protect, requireRole("OWNER"), (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "You are an owner",
+    user: req.user,
+  });
+});
 export default router;
